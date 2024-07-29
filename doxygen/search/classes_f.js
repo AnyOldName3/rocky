b503d303c112a0b80e215cec407a222f9b7f10dc
AnyOldName3 <AnyOldName3@users.noreply.github.com>
2024-07-29 16:01:39 +0000

    Deploying to gh-pages from @ AnyOldName3/rocky@a3c0a2f2bd701335140734e0f3198295ac86732a 🚀

--- doxygen/search/classes_f.js
+++ doxygen/search/classes_f.js
@@ -5,7 +5,7 @@ var searchData=
   ['srs_944',['SRS',['../class_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1_s_r_s.html',1,'ROCKY_NAMESPACE']]],
   ['srsoperation_945',['SRSOperation',['../class_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1_s_r_s_operation.html',1,'ROCKY_NAMESPACE']]],
   ['stats_946',['Stats',['../struct_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1_application_1_1_stats.html',1,'ROCKY_NAMESPACE::Application']]],
-  ['status_947',['Status',['../class_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1_status.html',1,'ROCKY_NAMESPACE']]],
+  ['status_947',['Status',['../struct_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1_status.html',1,'ROCKY_NAMESPACE']]],
   ['stream_948',['Stream',['../struct_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1_u_r_i_1_1_stream.html',1,'ROCKY_NAMESPACE::URI']]],
   ['stylesheet_949',['StyleSheet',['../struct_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1_style_sheet.html',1,'ROCKY_NAMESPACE']]],
   ['surfacenode_950',['SurfaceNode',['../class_r_o_c_k_y___n_a_m_e_s_p_a_c_e_1_1_surface_node.html',1,'ROCKY_NAMESPACE']]],
